7a99da309dea83a3843d24ba46013ab21fec7c0e
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-15 23:28:37 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+//Array of objects
+const quiz = [
+    {
+        q:"He's such a kind, ............... man; so popular with his workmates.",
+        options:["generous", "wicked and mean", "aimless", "hostile", "outrageous"],
+        answer: 0
+    },
+    {
+        q:"Food kept too long outside a fridge will ............... through the action of moulds and bacteria on it.",
+        options:["pull apart", "go off", "be out", "throw out", "turn down"],
+        answer: 1
+    },
+    {
+        q:"She is the ............... daughter of a prominent Turkish politician. Her real parents had been killed in a car crash when she was little.",
+        options:["saved", "adapted", "adopted", "refused", "accustomed"],
+        answer: 2
+    },
+    {
+        q:"People ............... for all sorts of reasons, but a desire to slim down tops the list.",
+        options:["fit into", "run out", "dine", "diet", "fight"],
+        answer: 3
+    }
+]
